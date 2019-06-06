48690e3a67fbe9c417ec7eb54f099f410ce26605
Brian DiRito <bdirito91@gmail.com>
2019-06-06 13:32:10 -0400

    data_viz almost complete

--- data_viz_project/data_viz.py
+++ data_viz_project/data_viz.py
@@ -5,8 +5,10 @@ need!
 '''
 
 import json
-from textblob import TextBlob
 import matplotlib.pyplot as plt
+
+from textblob import TextBlob
+from wordcloud import WordCloud
 
 #Get the JSON data
 tweetFile = open("./TwitterData/tweets_small.json", "r")
@@ -18,8 +20,11 @@ polarity_list = []
 subjectivity_list = []
 polarity_total = 0
 subjectivity_total = 0
+tweets_string = ""
 
 for twt in tweet_data:
+    tweets_string += twt["text"]
+
     twt_tb = TextBlob(twt["text"])
     polarity_list.append(twt_tb.sentiment.polarity)
     polarity_total += twt_tb.sentiment.polarity
@@ -31,3 +36,44 @@ subjectivity_avg = subjectivity_total/len(subjectivity_list)
 
 print("\nThe average polarity is: {avg}\n".format(avg=polarity_avg))
 print("The average subjectivity is: {avg}\n".format(avg=subjectivity_avg))
+
+# Polarity Histogram
+# plt.hist(polarity_list, bins=[-1, -0.5, 0.0, 0.5, 1])
+# plt.xlabel("Polarities")
+# plt.ylabel("Number of Tweets")
+# plt.title("Polarity Histogram")
+# plt.axis([-1.1, 1.1, 0, 100])
+# plt.grid(True)
+#
+# print("Polarities Histogram ready to be viewed")
+#
+# plt.show()
+#
+# # Sentimentality Histogram
+# plt.hist(subjectivity_list, bins=[-1, -0.5, 0.0, 0.5, 1])
+# plt.xlabel("Subjectivities")
+# plt.ylabel("Number of Tweets")
+# plt.title("Subjectivities Histogram")
+# plt.axis([-1.1, 1.1, 0, 100])
+# plt.grid(True)
+#
+# print("\nSubjectivities Histogram ready to be viewed\n")
+#
+# plt.show()
+
+tweets_blob = TextBlob(tweets_string)
+word_count = tweets_blob.word_counts
+common_words = ["and", "about", "the", "http"]
+filtered_words = {}
+
+for word in word_count:
+    if word.isalpha() and word not in common_words:
+        if word.lower() in filtered_words:
+            filtered_words[word.lower()] += 1
+        else:
+            filtered_words[word.lower()] = 1
+
+wordcloud = WordCloud().generate_from_frequencies(filtered_words)
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
