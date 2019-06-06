fb7e5862d83ff19f9ffb8667d649f81613e3deb2
Brian DiRito <bdirito91@gmail.com>
2019-06-06 12:47:17 -0400

    completed part 1 data viz project

--- data_viz_project/data_viz.py
+++ data_viz_project/data_viz.py
@@ -9,13 +9,25 @@ from textblob import TextBlob
 import matplotlib.pyplot as plt
 
 #Get the JSON data
-tweetFile = open("TwitterData/tweets_small.json", "r")
-tweetData = json.load(tweetFile)
+tweetFile = open("./TwitterData/tweets_small.json", "r")
+tweet_data = json.load(tweetFile)
 tweetFile.close()
-tweetData.append("hello world")
-print(tweetData)
-# Continue your program below!
 
-# Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+# Continue your program below!
+polarity_list = []
+subjectivity_list = []
+polarity_total = 0
+subjectivity_total = 0
+
+for twt in tweet_data:
+    twt_tb = TextBlob(twt["text"])
+    polarity_list.append(twt_tb.sentiment.polarity)
+    polarity_total += twt_tb.sentiment.polarity
+    subjectivity_list.append(twt_tb.sentiment.subjectivity)
+    subjectivity_total += twt_tb.sentiment.subjectivity
+
+polarity_avg = polarity_total/len(polarity_list)
+subjectivity_avg = subjectivity_total/len(subjectivity_list)
+
+print("\nThe average polarity is: {avg}\n".format(avg=polarity_avg))
+print("The average subjectivity is: {avg}\n".format(avg=subjectivity_avg))
