cc6e3ce5909704de917f4da4eca902638eacfb76
Brian DiRito <bdirito91@gmail.com>
2019-06-06 12:22:32 -0400

    started data viz project

---
+++ data_viz_project/data_viz.py
@@ -0,0 +1,21 @@
+'''
+In this project, you will visualize the feelings and language used in a set of
+Tweets. This starter code loads the appropriate libraries and the Twitter data you'll
+need!
+'''
+
+import json
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+
+#Get the JSON data
+tweetFile = open("TwitterData/tweets_small.json", "r")
+tweetData = json.load(tweetFile)
+tweetFile.close()
+tweetData.append("hello world")
+print(tweetData)
+# Continue your program below!
+
+# Textblob sample:
+tb = TextBlob("You are a brilliant computer scientist.")
+print(tb.polarity)
