794b8fe2ff651eb221be7f12702b0524fc6fa3df
Brian DiRito <bdirito91@gmail.com>
2019-06-14 14:43:12 -0400

    halfway done with breakdown

---
+++ apiCodealong/codealong/partThree/partThree.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", function(){
+    let currentLocation = ol.proj.fromLonLat([-74.028106, 40.738729])
+    const homeLocation = ol.proj.fromLonLat([-74.028106, 40.738729])
+    const buttons = document.getElementsByClassName('buttons')[0]
+  
+    let view = new ol.View({
+      center: currentLocation,
+      zoom: 8
+    })
+  
+    let map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      loadTilesWhileAnimating: true,
+      view: view
+    })
+  
+    buttons.addEventListener("click", function(e) {
+      if (e.target.name === "panHomeButton") animateToLocation(homeLocation)
+    })
+  
+    function animateToLocation(loc){
+      view.animate({
+        center: loc,
+        duration: 2000
+      })
+    }
+  })
+  
