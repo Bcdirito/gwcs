719a0e2756eb4dc053dda0f4eba0127f39953ae4
Brian DiRito <bdirito91@gmail.com>
2019-07-16 19:55:36 -0400

    added day 3 data science review

---
+++ data_science_day_3_review.py
@@ -0,0 +1,37 @@
+# use the following list to test the functions:
+
+test_list = [1, 2, 3, 4, 5]
+
+# use the following list of dictionaries to test the functions:
+
+test_dictionary_list = {
+    {"name": "brian", "position": "teacher"},
+    {"name": "edith", "position": "teacher"},
+    {"name": "thomas", "position": "site lead"},
+    {"name": "shine", "position": "partner"},
+    {"name": "julie", "position": "partner"}
+}
+
+# given a list of integers, find the average of the integers
+# return the value
+# print the function
+
+def list_average(lst):
+    pass
+
+# given a list of integers, return a list of the odd numbers in the list
+# print the function
+
+def is_odd(lst):
+    pass
+
+# given a list of dictionaries, return a a list of the partners
+# print the function
+
+def is_partner(lst):
+    pass
+
+print(list_average(test_list))
+
+# utilizing the example above, print the other two functions
+
