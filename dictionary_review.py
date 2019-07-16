427590389fda4b1ffc4afa648aaa3544f71397ac
Brian DiRito <bdirito91@gmail.com>
2019-07-15 22:52:25 -0400

    added dictionary review

---
+++ dictionary_review.py
@@ -0,0 +1,30 @@
+# declare an empty dictionary
+# there should be two key/value pairs. they can be whatever you want them to be.
+# then print the second dictionary pair value
+# ex: sample_dictionary = {
+#   "key_one": "value_one",
+#   "key_two": False
+# }
+# print(sample_dictionary["key_two"])
+
+dictionary_list = [
+    {"name": "brian"},
+    {"name": "edith"},
+    {"name": "thomas"},
+    {"name": "shine"},
+    {"name": "julie"}
+]
+
+# given the above list, print the name value of each object
+# use a for loop
+
+
+def dictionary_printer(names):
+    pass
+
+# write a function that adds the index of each object as an id in the dictionary
+def add_id(names):
+    pass
+    # how do i create a new key/value pair?
+
+# call both functions
