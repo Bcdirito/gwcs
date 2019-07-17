99e9355c4717ff53834737e996cc05cdad2a8eba
Brian DiRito <bdirito91@gmail.com>
2019-07-17 19:20:51 -0400

    added a python day 4 review

---
+++ data_science_day_4_review.py
@@ -0,0 +1,43 @@
+# use the following test list for the functions below
+
+test_list = [True, False, True, True, False]
+
+# define a function that counts how many times a loop runs before it hits a False value
+# break out of the loop once you reach a False boolean value
+# print the counter
+
+def truth_counter(lst):
+    pass
+
+# define a function that makes a new list of just the truth values
+# print the length of the list
+# return the list
+# print the function
+
+def new_truth_list(lst):
+    pass
+
+
+# use the following list for the function below
+test_names_list = ["brian", "brian", "edith", "edith", "brian"]
+
+# define a function that returns a dictionary where the keys are the two names and the values are how many times they appear in the function
+# print the function
+
+def name_counter_dictionary(lst):
+    pass
+
+# challenge:
+# use this dictionary
+test_nested_dictionary = {
+    "top_key": {
+        "mid_key": {
+            "bottom_key": "YEET!"
+        }
+    }
+}
+
+# using the dictionary above, print the value of "bottom_key"
+def dictionary_denester(dict):
+    pass
+
