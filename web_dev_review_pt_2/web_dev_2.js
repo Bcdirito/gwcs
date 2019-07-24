dabadc0d46e3057dc540bae0b3b5a41c1e62c02d
Brian DiRito <bdirito91@gmail.com>
2019-07-24 19:21:28 -0400

    created the review

---
+++ web_dev_review_pt_2/web_dev_2.js
@@ -0,0 +1,20 @@
+// How can we check that our JS file is linked to our HTML file?
+
+
+// Utilizing the function and array below, how can we return a list of all of the even numbers?
+
+const testArray = [0, 1, 2, 3, 4, 5]
+
+function getEvens(){
+
+}
+
+// How do we call a function?
+// How can we get the return to appear in our JS Chrome Console?
+
+// We want to add a click event to each of our headers that raises an alert with the phrase "THIS IS A HEADER Y'ALL!"
+
+    // What query selector do we need?
+    // What can we do to iterate through all of the elements?
+    // How can we convert our elements into an array?
+    // What is .forEach()?
