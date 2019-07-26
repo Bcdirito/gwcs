0fe0e2b4c309931c5bed8900353751d1d88b6ced
Brian DiRito <bdirito91@gmail.com>
2019-07-25 23:27:15 -0400

    added final js review

---
+++ web_dev_review_pt_3/web_dev_3.js
@@ -0,0 +1,8 @@
+// Using the variables below, how can we add a hover event where we change our loremText to the revLorem text?
+
+// How can we change it back through leaving on our mouse?
+
+let loremText = document.getElementById("lorem").innerText
+
+const revLorem = "sequi Culpa corporis natus commodi doloribus exercitationem praesentium quidem itaque nam tenetur rem est alias doloremque earum maxime perferendis quaerat fugiat Dolorem elit adipisicing consectetur amet sit dolor ipsum Lorem"
+
